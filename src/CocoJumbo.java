57a3688fc77b508600b40c38f5ceba26f0e1aabf
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-11-12 10:23:33 -0600

    added lines of code

--- src/CocoJumbo.java
+++ src/CocoJumbo.java
@@ -1,2 +1,6 @@
 public class CocoJumbo {
+    public static void main(String[] args) {
+        System.out.println("Check your head!");
+        System.out.println("Where is your head at?!");
+    }
 }
